424e9746e4f63e94cc1cbdf9670d6cce76cc3943
Agriculture <shivamjswl13@gmail.com>
2024-10-11 23:46:11 +0530

    success

--- Routes/index.js
+++ Routes/index.js
@@ -2,6 +2,38 @@ const express=require('express');
 const fs=require('fs');
 const app=express();
 const users=require('./MOCK_DATA.json');
+const mongoose=require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/Learning-Mongodb').
+then(()=> console.log("MongoDB Connected"))
+.catch(err=> console.log("Error Occured",err))
+
+
+const userSchema= new mongoose.Schema({
+    first_name:{
+        type: String,
+        require: true,
+    },
+    last_name:{
+        type: String,
+        require:false,
+    },
+    email:{
+        type: String,
+        require: true,
+        unique: true,
+    },
+    jobtitle:{
+        type: String,
+    },
+    gender:{
+        type: String,
+    }
+
+});
+
+const User=mongoose.model("model",userSchema);
+
 app.route("/api/user/:id")
 .get((req,res)=>{
     const id=Number(req.params.id);
@@ -10,21 +42,31 @@ app.route("/api/user/:id")
 })
 
 app.use(express.urlencoded({extended: false}));
-app.post("/api/user",(req,res)=>{
+app.post("/api/user",async(req,res)=>{
     const body=req.body;
-    users.push({...body,id:users.length+1});
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
-        return res.json({status: "Pending",id: users.length});
-    });
+    if(!body.first_name||
+        !body.last_name||
+        !body.email||
+        !body.gender
+    ){
+        return res.status(400).json({msg:"All fields are required"});
+    }
+   const result= await User.create({
+        first_name: body.first_name,
+        last_name: body.last_name,
+        email: body.email,
+        gender: body.gender,
+    })
     
+    return res.status(200).json({msg:"Success"});
    
 })
 
-app.get("/user",(req,res)=>{
-    const html=`
-    <ul>
-      ${users.map(user=>`<li>${user.first_name}</li>`).join("")}
-    </ul>`;
-    res.send(html);
-})
+// app.get("/user",(req,res)=>{
+//     const html=`
+//     <ul>
+//       ${users.map(user=>`<li>${user.first_name}</li>`).join("")}
+//     </ul>`;
+//     res.send(html);
+// })
 app.listen(8000,()=> console.log("Server Started Succesfully"));
