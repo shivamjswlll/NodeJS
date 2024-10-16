154a7fe7864562e33a9c669d698b3ed4b0f7f676
Agriculture <shivamjswl13@gmail.com>
2024-10-17 02:05:14 +0530

    update

--- SHORT-URL/Controllers/url.js
+++ SHORT-URL/Controllers/url.js
@@ -1,10 +1,10 @@
 const shortid=require('shortid');
-const URL=require('../models/url');
+const {URL}=require('../models/url');
 
 async function handleGenerateNewShortURL(req,res){
     const body=req.body;
     if(!body.url) return res.status(400).json({error:'url is required'})
-    const shortID=shortid(8);
+    const shortID=shortid.generate();
     await URL.create({
         shortId: shortID,
         redirectURL: body.url,
--- SHORT-URL/index.js
+++ SHORT-URL/index.js
@@ -2,10 +2,28 @@ const express=require('express');
 const {connectMongoDB}=require('./connect');
 const urlroute=require('./routes/url')
 const app=express();
+const {URL}=require('./models/url')
 
 connectMongoDB('mongodb://localhost:27017/short-url').
 then(console.log('MongoDB connected'));
 const PORT=8001;
+
+app.use(express.json());
+app.use('/url',urlroute);
+app.get('/:shortId',async (req,res)=>{
+    const shortId=req.params.shortId;
+   const entry= await URL.findOneAndUpdate(
+        {
+        shortId
+    },
+    {
+        $push: {
+        visitHistory:{timestamp: Date.now()},
+    },
+    })
+    res.redirect(entry.redirectURL);
+})
+
 
 
 app.listen(PORT,()=> console.log(`Server started at PORT ${PORT}`));
