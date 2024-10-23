7b7ad8828131e7ee37d9484ff484668916ffed49
Agriculture <shivamjswl13@gmail.com>
2024-10-23 14:50:48 +0530

    Learning

--- SHORT-URL/routes/staticrouter.js
+++ SHORT-URL/routes/staticrouter.js
@@ -1,8 +1,12 @@
 const express=require('express');
+const URL = require('../models/url');
 
 const router=express.Router();
 
-router.get('/',(req,res)=>{
-    res.render('home')
+router.get('/',async(req,res)=>{
+    const allurls=URL.find({});
+    res.render('home',{
+        url: allurls,
+    })
 })
 module.exports=router;
